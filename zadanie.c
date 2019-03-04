f1d2b27072a6e2a8b25a980c20c9ed6289f23d7d
nikola.gabrisova <nikolagabrisova99@gmail.com>
2019-03-04 22:35:35 +0100

    vyjadrenie a vypisanie prieniku

--- zadanie.c
+++ zadanie.c
@@ -12,9 +12,35 @@ void nastav_pole(int x,int pole[x])
 		}
 }
 
+void vypis_pole(int x,int pole[x])
+{
+	int i;
+	for(i=0;i<x;i++)
+		{
+			printf("%d ",pole[i]);
+		}
+	printf("\n");
+}
+
+void prienik(int pole1[n],int pole2[m])
+{
+	int i,j;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<m;j++)
+		{
+			if (pole1[i]==pole2[j])
+				printf("%d " ,pole2[j]);
+		}
+	}
+}				 
 int main()
 {
 	int A[n],B[m];
 	nastav_pole(n,A);
 	nastav_pole(m,B);
+	vypis_pole(n,A);
+	vypis_pole(m,B);
+	prienik(A,B);
+	
 }
