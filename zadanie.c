134c6a22e9fc2880dbbe93da08b50b5e09d016c7
nikola.gabrisova <nikolagabrisova99@gmail.com>
2019-03-09 20:32:59 +0100

    zmena nacitavania mnozin

--- zadanie.c
+++ zadanie.c
@@ -1,46 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define n 5
-#define m 3
+#define m 5
 
 void nastav_pole(int x,int pole[x])
 {
 	int i;
+	printf("zadaj prvky mnoziny:\n");
 	for(i=0;i<x;i++)
 		{
-			pole[i]=1+rand()%10;
-		}
-}
-
-void vypis_pole(int x,int pole[x])
-{
-	int i;
-	for(i=0;i<x;i++)
-		{
-			printf("%d ",pole[i]);
+			scanf("%d",&pole[i]);
 		}
 	printf("\n");
 }
 
-void prienik(int pole1[n],int pole2[m])
+void prienik(int x,int y,int pole1[x],int pole2[y])
 {
 	int i,j;
-	for(i=0;i<n;i++)
+	printf("prienik tvojich mnozin:\n");
+	for(i=0;i<x;i++)
 	{
-		for(j=0;j<m;j++)
+		for(j=0;j<y;j++)
 		{
 			if (pole1[i]==pole2[j])
 				printf("%d " ,pole2[j]);
 		}
 	}
-}				 
+	printf("\n");
+}	
+
 int main()
 {
 	int A[n],B[m];
 	nastav_pole(n,A);
 	nastav_pole(m,B);
-	vypis_pole(n,A);
-	vypis_pole(m,B);
-	prienik(A,B);
-	
+	prienik(n,m,A,B);
+	return(0);
 }
