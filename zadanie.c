e68e9c99dfce0e8d7467b26707fc9352672057d6
nikola.gabrisova <nikolagabrisova99@gmail.com>
2019-06-04 00:28:42 +0200

    uprava-prienik a zjednotenie

--- zadanie.c
+++ zadanie.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 typedef struct{
@@ -7,74 +7,112 @@ typedef struct{
 	unsigned int dlzka;
 }MNC;
 
-void prienik(MNC *A,MNC *B)
+void vypis_mnoziny(MNC *C)
+{
+	int i;
+	
+	for(i=0;i<C->dlzka;i++)
+		printf("%d ",C->pole[i]);
+	
+	printf("\n");
+}
+	
+
+void prienik(MNC *A,MNC *B,MNC *C)
 {
 	int i,j;
+	unsigned int pocet=0; 
 	
-	printf("Prienik mnozin:\n");
 	for(i=0;i<A->dlzka;i++)
 	{
 		for(j=0;j<B->dlzka;j++)
 		{
 			if (A->pole[i]==B->pole[j])
-				printf("%d " ,B->pole[j]);
+			{
+			
+				C->pole[pocet]=B->pole[j];
+				pocet++;
+			}
 		}
 	}
-	printf("\n");
+	C->dlzka=pocet;
+	C->pole=&(C->pole[0]);
 }
 
-void zjednotenie(MNC *A, MNC *B)
+void zjednotenie(MNC *A, MNC *B,MNC *C)
 { 	
-	int i = 0, j = 0; 
+	int i = 0, j = 0;
+	unsigned int pocet=0; 
 	
-	printf("Zjednotenie mnozin:\n");
 	while (i < A->dlzka && j < B->dlzka) 
 	{ 
 		if (A->pole[i] < B->pole[j]) 
 		{
-			printf(" %d ", A->pole[i]); 
+			C->pole[pocet]=A->pole[i]; 
 			i++;
+			pocet++;
 		}
 		else if (B->pole[j] < A->pole[i]) 
 		{
-			printf(" %d ", B->pole[j]); 
+			C->pole[pocet]=B->pole[j]; 
 			j++;
+			pocet++;
 		}
 		else
 		{	 
-			printf(" %d ", B->pole[j]); 
+			C->pole[pocet]=A->pole[i]; 
 			i++;
 			j++; 
+			pocet++;
 		} 
 	}
    
-	while(i < A->dlzka) 
+	if(i==A->dlzka && pocet==A->dlzka) 
 	{
-		printf(" %d ", A->pole[i]);
-		i++;
+		while (j<B->dlzka)
+		{
+			C->pole[pocet]=B->pole[j];
+			pocet++;
+			j++;
+		}
 	}
 	 
-	while(j < B->dlzka) 
+	else
 	{
-		printf(" %d ", B->pole[j]);
-		j++;
+		while (i<A->dlzka)
+		{
+			C->pole[pocet]=A->pole[i];
+			pocet++;
+			i++;
+		}
 	}
-}
 	
+	C->dlzka=pocet;
+	C->pole=&(C->pole[0]);
+}
 
-int main()
+
+
+int main(void)
 {
     
-	int A[5]={1,2,3,4,5},B[5]={3,4,5,6,7};
-	MNC N,M;
-	
+	int A[8]={1,2,3,4,5,6,7,8},B[5]={7,8,9,10,11},P[8]={},Z[13]={};
+	MNC N,M,C,D;
+
 	N.pole=A;
-	N.dlzka=5;
+	N.dlzka=8;
 	M.pole=B;
 	M.dlzka=5;
+	C.pole=P;
+	D.pole=Z;
 	
-	prienik(&N,&M);
-	zjednotenie(&N,&M);
+	prienik(&N,&M,&C);
+	printf("\nPrienik mnozin:\n");
+	vypis_mnoziny(&C);
 	
+	zjednotenie(&N,&M,&D);
+	printf("\nZjednotenie mnozin:\n");
+	vypis_mnoziny(&D);
+
 	return(0);
 }
